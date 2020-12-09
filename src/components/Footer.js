5ccce8bf0015ae16eccfa9b9328779e9c43a8ac7
Carlos Fernandez <jcfernan@live.com>
2020-12-09 00:06:32 -0700

    Update to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,24 @@ export default class Footer extends Component {
             <footer>
                 <div className= "row">
                     <div className= "twelve columns">
-                        <ul className= 'social'
+                        <ul className= 'social-links'>
+                            {
+                                resumeData.socialLinks && resumeData.socialLinks.map((item) =>{
+                                    return(
+                                        <li>
+                                            <a href= {item.url}>
+                                                <i className= {item.className}/>
+                                            </a>
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
                     </div>
+                    <div id="go-top"><a className= "smoothScroll" title= "Back to Top"
+                    href= "#home"><i className= "icon-up-open"/></a></div>
                 </div>
             </footer>
-        )
+        );
     }
 }
