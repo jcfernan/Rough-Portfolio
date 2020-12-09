dfde5e2bc7ad9cc85d4dab663c2586cea56d8ac8
Carlos Fernandez <jcfernan@live.com>
2020-12-08 23:51:11 -0700

    Added linkedin to contact

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,1 +1,26 @@
 import React, {Component} from 'react';
+export default class ContactUs extends Component{
+    render(){
+        let resumeData = this.resumeData;
+        return(
+            <section id ="contact">
+                <div className= "row section-head">
+                    <div className= "ten columns">
+                        <p className= "lead">
+                            Please fell free to contact me for any collaborations or meetups below 
+                        </p>
+                    </div>
+                </div>
+                <div className= "row">
+                    <aside className= "eight columns footer-widget">
+                        <div className= "widget">
+                            <h4>Linkedin:
+                                {resumeData.linkedinId}
+                            </h4>
+                        </div>
+                    </aside>
+                </div>
+            </section>
+        );
+    }
+}
