6cbf70f45a08635df3646a5fa5fe751dd777f11a
Carlos Fernandez <jcfernan@live.com>
2020-12-09 00:00:33 -0700

    Started the footer links

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+export default class Footer extends Component {
+    render(){
+        let resumeData = this.props.resumeData;
+        return(
+            <footer>
+                <div className= "row">
+                    <div className= "twelve columns">
+                        <ul className= 'social'
+                    </div>
+                </div>
+            </footer>
+        )
+    }
+}
