f726b6eee24a473a49377a4cfa6d83946771e2e8
Carlos Fernandez <jcfernan@live.com>
2020-12-14 17:18:12 -0700

    Work on adding blog display

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -3,5 +3,36 @@ import React, {Component} from 'react';
 export default class Blog extends Component {
     render(){
         let resumeData = this.props.resumeData;
+        return(
+            <section id= "blog">
+                <div className= "text-container">
+                <div className= "row">
+                    <div className= "two columns header-col">
+                        <h1><span>Blog Posts</span></h1>
+                    </div>
+                    <div className= "ten columns flex-container">
+                        <div className= "flexslider">
+                            <ul className= "slides">
+                                {
+                                    resumeData.blogPosts && resumeData.blogPosts.map((item) => {
+                                        return(
+                                            <li>
+                                                <blockquote>
+                                                    <p>
+                                                        {item.description}
+                                                    </p>
+                                                    <cite>{item.name}</cite>
+                                                </blockquote>
+                                            </li>
+                                        )
+                                    })
+                                }
+                            </ul>
+                        </div> {/*div.flexslider ends */}
+                    </div> {/*div.flex-container ends */}
+                 </div> {/*row ends} */}
+                </div> {/* text-container ends */}
+            </section>
+        );
     }
 }
