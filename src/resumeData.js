8a138cb6d8037995d082b285d4fa21c26c048af6
Carlos Fernandez <jcfernan@live.com>
2020-12-16 16:19:17 -0700

    update resume tree

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,3 +1,13 @@
 let resumeData= {
-    
+    "imagebaseurl":"https://https://avatars1.githubusercontent.com/u/61516018?s=460&u=b4a8b9b86b8c1e723f7c2e4d8b049839bed36e78&v=4",
+    "name":"Carlos Fernandez",
+    "role":"Full Stack Developer",
+    "linkedinId":"cfenan",
+    "githubId":"jcfernan",
+    "roleDescription":"Full Stack Developer with a background in Natural Resource Management and Leadership. Experienced in Javascript | Ruby | React | SQLite | Python | Ruby on Rails | HTML | CSS | Adobe Photoshop",
+    "socialLinks":[
+        {
+            
+        }
+    ]
 }
